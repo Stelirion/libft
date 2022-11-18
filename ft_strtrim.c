3d1ad48c6fd1a3dc7f1f7208f6ca728c110db852
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2022-11-18 11:43:41 +0100

    all fix, rest split strmapi itoa and striteri

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:46:00 by ngennaro          #+#    #+#             */
-/*   Updated: 2022/11/17 13:29:41 by ngennaro         ###   ########lyon.fr   */
+/*   Updated: 2022/11/18 11:33:32 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,22 @@ static int	to_trim(char c, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		i;
-	int		start;
-	int		end;
+	size_t		i;
+	size_t		start;
+	size_t		end;
 
 	i = 0;
 	start = 0;
 	end = 0;
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
 	while (to_trim(s1[start], set))
 		start++;
-	while (s1[i + 1])
+	if (start == ft_strlen(s1))
+		return (ft_strdup(""));
+	while (s1[i])
 		i++;
+	i--;
 	while (to_trim(s1[i--], set))
 		end ++;
 	return (ft_substr(s1, start, (ft_strlen(s1) - start - end)));
