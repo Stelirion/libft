aec6bb8368191554cd780f2b377b4fc7d06ff55c
ngennaro <ngennaro@z1r12p1.42lyon.fr>
2022-11-23 13:39:44 +0100

    finish

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,26 @@
 /*   By: ngennaro <ngennaro@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:51:43 by ngennaro          #+#    #+#             */
-/*   Updated: 2022/11/23 08:46:28 by ngennaro         ###   ########lyon.fr   */
+/*   Updated: 2022/11/23 13:31:22 by ngennaro         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+char	**free_all(char **tab, size_t line)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= line)
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -22,33 +37,39 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	words = 0;
+	if (!s)
+		return (NULL);
+	
 	while (s[i])
 	{
 		if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
 			words++;
 		i++;
 	}
-	tab = malloc(sizeof(char *) * words + 1);
+	tab = malloc(sizeof(char *) * (words + 1));
 	if (!tab)
 		return (NULL);
-	tab[words + 1] = ft_strdup("NULL");
 	i = 0;
 	words = 0;
 	while (s[i])
 	{
-		if (s[i] != c)
+		if (s[i] == c)
+			i++;
+		else if (s[i] != c)
 		{
 			size = 0;
 			start = i;
-			while (s[i] != c)
+			while (s[i] && s[i] != c)
 			{
 				i++;
 				size++;
 			}
 			tab[words] = ft_substr(s, start, size);
+			if (!tab[words])
+				return (free_all(tab, words));
 			words++;
 		}
-		i++;
 	}
-	return(tab);
+	tab[words] = NULL;
+	return (tab);
 }
